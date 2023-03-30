1eea077324acf5f86572c96ba9ed70f3069d3618
wantSome <kihyun1998@naver.com>
2023-03-30 16:10:26 +0900

    start spring boot

---
+++ src/main/java/com/maker/Smart_To_Do_List/SmartToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.maker.Smart_To_Do_List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartToDoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maker/Smart_To_Do_List/SmartToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maker.Smart_To_Do_List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
