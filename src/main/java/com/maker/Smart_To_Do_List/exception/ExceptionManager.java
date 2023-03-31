60eb6f81ce1a4ef1537a5b3e437a8a38ddd9581a
wantSome <kihyun1998@naver.com>
2023-03-31 21:21:29 +0900

    Create ExceptionManager.java

---
+++ src/main/java/com/maker/Smart_To_Do_List/exception/ExceptionManager.java
@@ -0,0 +1,17 @@
+package com.maker.Smart_To_Do_List.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionManager {
+    
+    // RuntimeException 발생 시 HttpStatus를 Confluct과 메시지 return
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> runtimeExceptionHandler(RuntimeException e){
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(e.getMessage());
+    }
+}
