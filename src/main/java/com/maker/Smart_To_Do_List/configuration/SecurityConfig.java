c9ca9a8f09d24ede0a8b1c277a9ba2452a9d150a
wantSome <kihyun1998@naver.com>
2023-04-04 19:13:06 +0900

    인증 인가 기능 추가 - 1

--- src/main/java/com/maker/Smart_To_Do_List/configuration/SecurityConfig.java
+++ src/main/java/com/maker/Smart_To_Do_List/configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.maker.Smart_To_Do_List.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,8 +20,8 @@ public class SecurityConfig {
                 .csrf().disable() // csrf공격 disable
                 .cors().and() // cors 허용
                 .authorizeRequests()
-                .antMatchers("/api/**").permitAll() // 전체 허용
-                .antMatchers("/api/v1/users/join","/api/v1/users/login").permitAll() // 이 부분을 더 허용
+                .antMatchers("/api/v1/users/join","/api/v1/users/login").permitAll() // 토큰 없이 허용
+                .antMatchers(HttpMethod.POST,"/api/v1/**").authenticated() // 토큰 받아야 허용
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //jwt 사용할 때 씀
