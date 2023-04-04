38c6b0b738fe260c65f59d18e13071743e9a7ab6
wantSome <kihyun1998@naver.com>
2023-04-04 20:50:03 +0900

    로그인 테스트 성공

--- src/main/todo-list/src/route/Menu.jsx
+++ src/main/todo-list/src/route/Menu.jsx
@@ -10,7 +10,7 @@ const Menu = () => {
       <Link to={"/"}><h3>메인</h3></Link>
       <Link to={"/intro"}><h3>소개</h3></Link>
       <Link to={"/users/login"}><h3>로그인</h3></Link>
-      <Link to={"/register"}><h3>회원가입</h3></Link>
+      <Link to={"/users/join"}><h3>회원가입</h3></Link>
       <Link to={"/account"}><h3>계정</h3></Link>
     </div>
   );
