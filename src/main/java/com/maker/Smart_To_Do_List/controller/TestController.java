400feb9938e57bb66917e6d54143d5ee44766582
wantSome <kihyun1998@naver.com>
2023-04-04 17:15:51 +0900

    sorry

--- src/main/java/com/maker/Smart_To_Do_List/controller/TestController.java
+++ src/main/java/com/maker/Smart_To_Do_List/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
     // 연동 테스트 컨트롤러
-    
+
     @GetMapping("/nowij")
     public String Nowij() {
         return "Spring Boot and React 연동 테스트 \n";
