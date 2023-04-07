89b9f984d1c92d4eeb6f44ae47e9df9e8b450e9e
wantSome <kihyun1998@naver.com>
2023-04-07 16:31:52 +0900

    Revert "user 바꾸는 중.."
    This reverts commit cf4fdfd9f0c28c04e681ac2e07c43d1fa14ef3ac.

--- src/main/java/com/maker/Smart_To_Do_List/controller/UserController.java
+++ src/main/java/com/maker/Smart_To_Do_List/controller/UserController.java
@@ -6,7 +6,10 @@ import com.maker.Smart_To_Do_List.dto.LoginRequest;
 import com.maker.Smart_To_Do_List.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,7 +18,7 @@ public class UserController {
 
     private final UserService userService;
 
-    @RequestMapping(value = "/join",method = RequestMethod.POST)
+    @PostMapping("/join")
     public ResponseEntity<String> join(@RequestBody JoinRequest joinDto){
 
         userService.join(
--- src/main/java/com/maker/Smart_To_Do_List/domain/User.java
+++ src/main/java/com/maker/Smart_To_Do_List/domain/User.java
@@ -14,28 +14,17 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
-@Table(name = "user", schema = "mydatabase", uniqueConstraints = {@UniqueConstraint(columnNames = "user_id")})
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id", unique = true, nullable = false)
     private Long userId;
 
-    @Column(name = "login_id",nullable = false)
     private String loginId;
-
-    @Column(name = "login_pw",nullable = false)
     private String loginPw;
-
-    @Column(name = "user_name",nullable = false)
     private String userName;
-
-    @Column(name = "created_at")
     @CreationTimestamp
     private Date createdAt;
-
-    @Column(name="user_email")
     private String userEmail;
 
 }
