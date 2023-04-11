8ccc545967d3518f6c49cd12231338ca052db23f
wantSome <kihyun1998@naver.com>
2023-04-11 18:35:27 +0900

    go back

--- src/main/java/com/maker/Smart_To_Do_List/configuration/SecurityConfig.java
+++ src/main/java/com/maker/Smart_To_Do_List/configuration/SecurityConfig.java
@@ -1,8 +1,6 @@
 package com.maker.Smart_To_Do_List.configuration;
 
 import com.maker.Smart_To_Do_List.service.UserService;
-import com.maker.Smart_To_Do_List.util.JwtUtil;
-import io.jsonwebtoken.Jwt;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -22,7 +20,7 @@ import javax.persistence.Id;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JwtUtil jwtUtil;
+    private final UserService userService;
     @Value("${jwt.secret}")
     private String secretKey;
 
@@ -35,12 +33,53 @@ public class SecurityConfig {
                 .cors().and() // cors 허용
                 .authorizeRequests()
                 .antMatchers("/api/v1/users/join","/api/v1/users/login").permitAll() // 토큰 없이 허용
-                .anyRequest().authenticated() // 토큰 받아야 허용
+                .antMatchers(HttpMethod.POST,"/api/v1/**").authenticated() // 토큰 받아야 허용
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //jwt 사용할 때 씀
                 .and()
-                .addFilterBefore(new JwtFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JwtFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
+//package com.maker.Smart_To_Do_List.configuration;
+//
+//import com.maker.Smart_To_Do_List.service.UserService;
+//import com.maker.Smart_To_Do_List.util.JwtUtil;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+//public class SecurityConfig {
+//
+//    private final UserService userService;
+//    @Value("${jwt.secret}")
+//    private String secretKey;
+//
+//    // 그냥 Spring.Security 사용하면 안되기 때문에 상속 받아서 재정의
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//        return httpSecurity
+//                .httpBasic().disable() // UI쪽으로 들어오는거 disable
+//                .csrf().disable() // csrf공격 disable
+//                .cors().and() // cors 허용
+//                .authorizeRequests()
+//                .antMatchers("/api/v1/users/join","/api/v1/users/login").permitAll() // 토큰 없이 허용
+//                .anyRequest().authenticated() // 토큰 받아야 허용
+//                .and()
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //jwt 사용할 때 씀
+//                .and()
+//                .addFilterBefore(new JwtFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class)
+//                .build();
+//    }
+//}
--- src/main/java/com/maker/Smart_To_Do_List/service/UserService.java
+++ src/main/java/com/maker/Smart_To_Do_List/service/UserService.java
@@ -4,8 +4,8 @@ import com.maker.Smart_To_Do_List.domain.User;
 import com.maker.Smart_To_Do_List.exception.AppException;
 import com.maker.Smart_To_Do_List.exception.ErrorCode;
 import com.maker.Smart_To_Do_List.repository.UserRepository;
-import com.maker.Smart_To_Do_List.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import com.maker.Smart_To_Do_List.utils.JwtUtil;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,13 +16,12 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder encoder;
-
     private final JwtUtil jwtUtil;
 
-<<<<<<< HEAD
+    @Value("${jwt.secret}")
+    private String key;
+    private Long expiroTimeMs = 1000 * 60 * 60l;
 
-=======
->>>>>>> parent of 99ae784 (refresh token 사용하는 방안 - 1)
     public String join(String loginId, String loginPw,String loginPwCheck, String userName, String userEmail){
 
         // 1. userName 중복 체크
@@ -32,19 +31,9 @@ public class UserService {
                     throw new AppException(ErrorCode.DUPLICATED, loginId + " is already exits");
                 });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-        // 2. 비밀번호 확인 검사 >> front에서 해결하는걸로
-//        if(!loginPw.equals(loginPwCheck)){
-//            throw new AppException(ErrorCode.INVALID_PASSWORD, "Password is not match !");
-//        }
-=======
-=======
->>>>>>> parent of 99ae784 (refresh token 사용하는 방안 - 1)
         if(!loginPw.equals(loginPwCheck)){
             throw new AppException(ErrorCode.INVALID_PASSWORD, "Password is not match !");
         }
->>>>>>> parent of 99ae784 (refresh token 사용하는 방안 - 1)
 
         // 2. save
         User user = User.builder()
@@ -59,22 +48,9 @@ public class UserService {
         return "SUCCESS";
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    @Transactional
-    public GlobalResDto login(String loginId, String loginPw){ // , HttpServletResponse response 보류
-        // 아이디 검사
-        User loginUser = userRepository.findByLoginId(loginId)
-=======
     public String login(String loginId, String loginPw){
         // NO LOGIN ID
         User selectedUser = userRepository.findByLoginId(loginId)
->>>>>>> parent of 99ae784 (refresh token 사용하는 방안 - 1)
-=======
-    public String login(String loginId, String loginPw){
-        // NO LOGIN ID
-        User selectedUser = userRepository.findByLoginId(loginId)
->>>>>>> parent of 99ae784 (refresh token 사용하는 방안 - 1)
                 .orElseThrow(()->new AppException(ErrorCode.NOT_FOUND, loginId + "is not found!!"));
 
         // INVALID PASSWORD
@@ -82,48 +58,10 @@ public class UserService {
             throw new AppException(ErrorCode.INVALID_PASSWORD, "The password is wrong.");
         }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-        // 토큰 생성 (createToken의 return값 수정해야 함)
-        TokenDto tokenDto = jwtUtil.createAllToken(loginUser.getLoginId());
-=======
-        // No Exception > token issuance
-        String token = JwtUtil.createToken(selectedUser.getLoginId() ,key,expiroTimeMs);
->>>>>>> parent of 99ae784 (refresh token 사용하는 방안 - 1)
-
-
-<<<<<<< HEAD
-        // refresh token 있으면 업데이트 없으면 생성
-        if(refreshToken.isPresent()){
-            RefreshToken updatedToken = refreshToken.get();
-            updatedToken.setRefreshToken(tokenDto.getRefreshToken());
-            refreshTokenRepository.save(updatedToken);
-        }else{
-            RefreshToken newToken = RefreshToken.builder()
-                    .refreshToken(tokenDto.getAccessToken())
-                    .userEmail(loginUser.getUserEmail())
-                    .build();
-            refreshTokenRepository.save(newToken);
-        }
-        
-        
-        //보류
-//        setHeader(response,tokenDto);
-
-        return new GlobalResDto("Login Success !!",HttpStatus.OK.value());
-    }
-
-    private void setHeader(HttpServletResponse response, TokenDto tokenDto){
-        response.addHeader(JwtUtil.ACCESS_TOKEN, tokenDto.getAccessToken());
-=======
-        return token;
->>>>>>> parent of 99ae784 (refresh token 사용하는 방안 - 1)
-=======
         // No Exception > token issuance
         String token = JwtUtil.createToken(selectedUser.getLoginId() ,key,expiroTimeMs);
 
 
         return token;
->>>>>>> parent of 99ae784 (refresh token 사용하는 방안 - 1)
     }
 }
