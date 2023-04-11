8fd150926ee05374ffc966714f37344b3b851434
wantSome <kihyun1998@naver.com>
2023-04-11 18:10:29 +0900

    go back

--- src/main/java/com/maker/Smart_To_Do_List/service/UserService.java
+++ src/main/java/com/maker/Smart_To_Do_List/service/UserService.java
@@ -1,38 +1,26 @@
 package com.maker.Smart_To_Do_List.service;
 
-import com.maker.Smart_To_Do_List.domain.RefreshToken;
 import com.maker.Smart_To_Do_List.domain.User;
-import com.maker.Smart_To_Do_List.dto.GlobalResDto;
-import com.maker.Smart_To_Do_List.dto.TokenDto;
 import com.maker.Smart_To_Do_List.exception.AppException;
 import com.maker.Smart_To_Do_List.exception.ErrorCode;
-import com.maker.Smart_To_Do_List.repository.RefreshTokenRepository;
 import com.maker.Smart_To_Do_List.repository.UserRepository;
 import com.maker.Smart_To_Do_List.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Value;
-
-import javax.servlet.http.HttpServletResponse;
-import javax.transaction.Transactional;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
-    private final RefreshTokenRepository refreshTokenRepository;
     private final BCryptPasswordEncoder encoder;
 
     private final JwtUtil jwtUtil;
 
 
-
-    @Transactional
-    public GlobalResDto join(String loginId, String loginPw, String loginPwCheck, String userName, String userEmail){
+    public String join(String loginId, String loginPw,String loginPwCheck, String userName, String userEmail){
 
         // 1. userName 중복 체크
         userRepository.findByLoginId(loginId)
@@ -41,47 +29,57 @@ public class UserService {
                     throw new AppException(ErrorCode.DUPLICATED, loginId + " is already exits");
                 });
 
+<<<<<<< HEAD
         // 2. 비밀번호 확인 검사 >> front에서 해결하는걸로
 //        if(!loginPw.equals(loginPwCheck)){
 //            throw new AppException(ErrorCode.INVALID_PASSWORD, "Password is not match !");
 //        }
+=======
+        if(!loginPw.equals(loginPwCheck)){
+            throw new AppException(ErrorCode.INVALID_PASSWORD, "Password is not match !");
+        }
+>>>>>>> parent of 99ae784 (refresh token 사용하는 방안 - 1)
 
-        // 3. email 중복 체크
-        userRepository.findByUserEmail(userEmail)
-                .ifPresent(user -> {
-                    throw new AppException(ErrorCode.DUPLICATED, userEmail + "is already exits");
-                });
-
-        // 4. save
+        // 2. save
         User user = User.builder()
                 .loginId(loginId)
-                .loginPw(encoder.encode(loginPw))  // 비밀번호 암호화
+                .loginPw(encoder.encode(loginPw))
                 .userName(userName)
                 .userEmail(userEmail)
                 .build();
 
         userRepository.save(user);
 
-        return new GlobalResDto("SUCCESS JOIN", HttpStatus.OK.value()) ;
+        return "SUCCESS";
     }
 
+<<<<<<< HEAD
     @Transactional
     public GlobalResDto login(String loginId, String loginPw){ // , HttpServletResponse response 보류
         // 아이디 검사
         User loginUser = userRepository.findByLoginId(loginId)
+=======
+    public String login(String loginId, String loginPw){
+        // NO LOGIN ID
+        User selectedUser = userRepository.findByLoginId(loginId)
+>>>>>>> parent of 99ae784 (refresh token 사용하는 방안 - 1)
                 .orElseThrow(()->new AppException(ErrorCode.NOT_FOUND, loginId + "is not found!!"));
 
-        // 비밀번호 검사
-        if(!encoder.matches(loginPw,loginUser.getLoginPw())){
+        // INVALID PASSWORD
+        if(!encoder.matches(loginPw,selectedUser.getLoginPw())){
             throw new AppException(ErrorCode.INVALID_PASSWORD, "The password is wrong.");
         }
 
+<<<<<<< HEAD
         // 토큰 생성 (createToken의 return값 수정해야 함)
         TokenDto tokenDto = jwtUtil.createAllToken(loginUser.getLoginId());
+=======
+        // No Exception > token issuance
+        String token = JwtUtil.createToken(selectedUser.getLoginId() ,key,expiroTimeMs);
+>>>>>>> parent of 99ae784 (refresh token 사용하는 방안 - 1)
 
-        // Refresh 토큰 확인
-        Optional<RefreshToken> refreshToken = refreshTokenRepository.findByUserEmail(loginUser.getUserEmail());
 
+<<<<<<< HEAD
         // refresh token 있으면 업데이트 없으면 생성
         if(refreshToken.isPresent()){
             RefreshToken updatedToken = refreshToken.get();
@@ -104,5 +102,8 @@ public class UserService {
 
     private void setHeader(HttpServletResponse response, TokenDto tokenDto){
         response.addHeader(JwtUtil.ACCESS_TOKEN, tokenDto.getAccessToken());
+=======
+        return token;
+>>>>>>> parent of 99ae784 (refresh token 사용하는 방안 - 1)
     }
 }
