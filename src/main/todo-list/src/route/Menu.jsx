2bf2bd717b797807d4a9437873180d743906516c
redbuttonking <sswedm123@naver.com>
2023-05-09 22:01:35 +0900

    내 정보, 메뉴 스타일 조정

--- src/main/todo-list/src/route/Menu.jsx
+++ src/main/todo-list/src/route/Menu.jsx
@@ -1,17 +1,24 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link,NavLink } from "react-router-dom";
 
 import styles from "./css/Menu.module.css";
 
 
 const Menu = () => {
+
+  const activeStyle = {
+    backgroundColor : "#606060",
+    color: "white"
+  }
+
+
   return (
     <div className={styles.menu}>
-      <Link to={"/"}><h3>메인</h3></Link>
-      <Link to={"/todo"}><h3>Todo 리스트</h3></Link>
-      <Link to={"/users/login"}><h3>로그인</h3></Link>
-      <Link to={"/users/join"}><h3>회원가입</h3></Link>
-      <Link to={"/account"}><h3>내정보</h3></Link>
+      <NavLink style={({isActive}) => (isActive ? activeStyle:{})} to={"/"}><h3>메인</h3></NavLink>
+      <NavLink style={({isActive}) => (isActive ? activeStyle:{})} to={"/todo"}><h3>Todo 리스트</h3></NavLink>
+      <NavLink style={({isActive}) => (isActive ? activeStyle:{})} to={"/users/login"}><h3>로그인</h3></NavLink>
+      <NavLink style={({isActive}) => (isActive ? activeStyle:{})} to={"/users/join"}><h3>회원가입</h3></NavLink>
+      <NavLink style={({isActive}) => (isActive ? activeStyle:{})} to={"/mypage"}><h3>내 정보</h3></NavLink>
     </div>
   );
 }
