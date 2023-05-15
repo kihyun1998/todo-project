fee5d4199700d52ca9e43f466cc8177f710f994e
Seang-G <mgw7730@gmail.com>
2023-05-16 00:38:44 +0900

    todo list 기능 추가
    - todo list 이름 수정, 삭제 기능 추가
    - todo list 관련 애니메이션 추가

--- src/main/todo-list/src/route/Menu.jsx
+++ src/main/todo-list/src/route/Menu.jsx
@@ -1,20 +1,13 @@
 import { useEffect, useState } from "react";
-import { NavLink, Link, useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { useCookies } from 'react-cookie';
 import styles from "./css/Menu.module.css";
 import axios from "axios";
 import styled from "styled-components";
+import { motion, useAnimationControls } from "framer-motion";
 
 import Button from "./css/component/Button";
-import Input from "./css/component/Input"
 import TodoList from "./css/component/TodoList";
-import todoList from "./css/component/TodoList";
-
-
-const Expend = styled.span`
-  cursor: pointer;
-  transform: rotate(${props=>props.clicked?"0":props.degree}deg);
-`
 
 const ToDoListInput = styled.input`
   margin-left: 35px;
@@ -26,6 +19,7 @@ const ToDoListAddBtn = styled.button`
   background-color: rgba(0, 0, 0, 0);
   border: 0px solid grey;
   height: 30px;
+  margin-left: 15px;
   // font-family: 'Sunflower', sans-serif;
   // font-weight: 1000
   color: green;
@@ -39,18 +33,19 @@ const activeStyle = {
 const Menu = () => {
   const [cookies, setCookie, removeCookie] = useCookies(["accessToken", "toDoLists"]);
   
-  const [expended, setExpended] = useState(true);
+  const [expended, setExpended] = useState(false);
   const [inputExpended, setInputExpended] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
   const [toDoListName, setToDoListName] = useState("");
   const [toDoLists, setToDoLists] = useState([]);
-  const [_, set_] = useState(0);
-  const navigate = useNavigate();
+
+  const controls = useAnimationControls();
 
   const tempLogin = () => setCookie("accessToken", "temp");
   const tempLogout = () => {
     removeCookie("accessToken");
     removeCookie("toDoLists");
-    navigate("/");
+    window.location.href = "/";
   }
   const onChangeToDoListName = (e) => setToDoListName(e.target.value);
 
@@ -101,87 +96,129 @@ const Menu = () => {
     getToDoListData();
   }, [cookies.accessToken])
 
-  const expendToDoList  = () => {
-    setExpended(pre=>!pre)
-  } 
+  const expendToDoList  = () => setExpended(pre=>!pre)
+  const expendInput = () => setInputExpended(pre=>!pre)
+  const edit = () => setIsEditing(pre=>!pre)
 
-  const expendInput = () => {
-    setInputExpended(pre=>!pre)
+  const expendTodoLists = async() => {
+    if(expended){
+      await controls.set({display:"block"})
+      controls.start({
+        height: "auto",
+      })
+    } else {
+      await controls.start({
+        height: "0px",
+        padding: "0px",
+      })
+      controls.set({display:"none"})
+    }
   }
+
+  useEffect(()=>{
+    expendTodoLists();
+  }, [controls, expended])
 
   return (
     <div className={styles.menu}>
       <NavLink style={({isActive}) => (isActive ? activeStyle:{})}  to={"/"}>메인</NavLink>
-      {cookies.accessToken==null?
-      null:
-      <div>
+      {cookies.accessToken!=null && (
         <div>
-          Todo 리스트
-          <Expend 
-            className={`material-symbols-outlined ${styles.expend}`} 
-            onClick={expendToDoList} 
-            clicked={expended}
-            degree="90">
-            expand_more
-          </Expend>
-          {expended?
-          <Expend 
-            className={`material-symbols-outlined ${styles.plus}`}
-            onClick={expendInput}
-            clicked={!inputExpended}
-            degree="45">
-            add
-          </Expend>:null
-          }
-          
-        </div>
-        {expended&&toDoLists?
-          <div>
-            {toDoLists.map((toDoList, idx) => 
-              <TodoList 
-                key={idx}
-                className={styles.todoList} 
-                to={`/todo/${toDoList.listId}`}
-                text={toDoList.listName}
-              />
+          <div style={{marginBottom:"15px"}}>
+            Todo 리스트
+            <motion.span 
+              className={`material-symbols-outlined ${styles.expend}`} 
+              onClick={expendToDoList} 
+              animate={{
+                rotate: expended ? 90:0,
+                transformOrigin: "15px 15px"
+              }}
+              whileHover={{
+                cursor:"pointer",
+                scale:1.2,
+              }}  
+            >
+              expand_more
+            </motion.span>
+            {expended && (
+              <motion.span 
+                className={`material-symbols-outlined ${styles.plus}`}
+                onClick={expendInput}
+                animate={{
+                  rotate: inputExpended ? 45:0,
+                  transformOrigin: "10px 10px"
+                }}
+                whileHover={{
+                  cursor:"pointer",
+                  scale:1.2,
+                }}
+              >
+                add
+              </motion.span>
             )}
-            {inputExpended?
-              <div>
-                <ToDoListInput 
-                  onChange={onChangeToDoListName}
-                  value={toDoListName}/>
-                <ToDoListAddBtn
-                  className={`material-symbols-outlined`}
-                  onClick={addToDoList}>
-                  done
-                </ToDoListAddBtn>
-              </div>
-              
-              :null}
+            {expended && (
+              <motion.span 
+                className={`material-symbols-outlined ${styles.edit}`}
+                onClick={edit}
+                animate={{
+                  rotate: isEditing ? -45:0,
+                  transformOrigin: "10px 10px"
+                }}
+                whileHover={{
+                  cursor:"pointer",
+                  scale:1.2,
+                }}
+              >
+                edit
+              </motion.span>
+            )}
           </div>
-          :null
-        }
-      </div>}
-      {cookies.accessToken==null?
-      null:
-      <NavLink style={({isActive}) => (isActive ? activeStyle:{})} to={"/mypage"}>내정보</NavLink>}
-      {cookies.accessToken==null?
-      null:
-      <NavLink to={"/"} onClick={tempLogout}>로그아웃</NavLink>
-      }
-      {cookies.accessToken==null?
-      <NavLink style={({isActive}) => (isActive ? activeStyle:{})} to={"/user/login"}>로그인</NavLink>:
-      null}
-      {cookies.accessToken==null?
-      <NavLink style={({isActive}) => (isActive ? activeStyle:{})} to={"/user/join"}>회원가입</NavLink>:
-      null}
+          {toDoLists&&(
+            <motion.div
+              style={{backgroundColor:isEditing?"#a3a3a3":"#f2f2f0", overflow:"hidden"}}
+              animate={controls}
+            >
+              {toDoLists.map((toDoList, idx) => 
+                <TodoList 
+                  key={idx}
+                  className={styles.todoList} 
+                  listId={toDoList.listId}
+                  text={toDoList.listName}
+                  isEditing={isEditing}
+                />
+              )}
+              {inputExpended&&(
+                <div>
+                  <ToDoListInput 
+                    onChange={onChangeToDoListName}
+                    value={toDoListName}/>
+                  <ToDoListAddBtn
+                    className={`material-symbols-outlined`}
+                    onClick={addToDoList}>
+                    done
+                  </ToDoListAddBtn>
+                </div>)
+                }
+            </motion.div>
+            )
+          }
+        </div>
+      )}
+      {cookies.accessToken!=null && (
+        <NavLink style={({isActive}) => (isActive ? activeStyle:{})} to={"/mypage"}>내정보</NavLink>)}
+      {cookies.accessToken!=null && (
+        <NavLink onClick={tempLogout}>로그아웃</NavLink>)}
+      {cookies.accessToken==null && (
+        <NavLink style={({isActive}) => (isActive ? activeStyle:{})} to={"/user/login"}>로그인</NavLink>)}
+      {cookies.accessToken==null && (
+        <NavLink style={({isActive}) => (isActive ? activeStyle:{})} to={"/user/join"}>회원가입</NavLink>)}
       
-      {cookies.accessToken==null?
-      <Button 
-        text={"로그인 상태 만들기 (메뉴상에서만)"}
-        onClick={tempLogin}
-      />:
-      null}
+      {cookies.accessToken==null && (
+        <Button 
+          text={"로그인 상태 만들기 (메뉴상에서만)"}
+          onClick={tempLogin}
+        />
+      )}
     </div>
   );
 }
--- src/main/todo-list/src/route/css/component/TodoList.jsx
+++ src/main/todo-list/src/route/css/component/TodoList.jsx
@@ -1,31 +1,157 @@
 import { NavLink } from "react-router-dom";
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+
+import Input from "./Input"
 import styled from "styled-components";
 
-const todoList = ({ className, to, text }) => {
-  const divStyle = {
-    display: "block",
-    paddingLeft: "35px",
-    fontSize: "25px",
-    marginTop: "5px",
-  }
-  const linkStyle = {
+const StyledDiv = styled.div`
+  ${props=>props.isEditing&&"display: flex;"}
+  // margin-top: 5px;
+  height: 33px;
+  font-size: 1.2rem;
+  justify-content: space-between;
+`
 
+const TodoList = ({ className, listId, text, isEditing }) => {
+
+  const [isEditingTitle, setIsEditingTitle] = useState(false);
+  const [ETitle, setETitle] = useState(text);
+
+  const linkStyle = {
+    display: "inline-block",
+    width: "90%",
+    height: "100%",
+    paddingLeft: "30px",
+    overflow: "hidden",
   }
+
+  const deleteStyle = {
+    color: "rgb(0, 0, 0)",
+    marginRight: "10px",
+    marginTop: "3px",
+    cursor:"pointer",
+  }
+
+  const listNameStyle = {
+    display: "inline-block",
+    width: "90%",
+    paddingLeft: "30px",
+    overflow: "hidden",
+    borderBottom: "0px solid #d6d6d6",
+    cursor:"pointer",
+  }
+
+  const editStyle = {
+    paddingLeft: "40px",
+    width: "150px",
+    height: "30px",
+    fontSize: "1.1rem",
+    margin: "0"
+  }
+
+  const checkStyle = {
+    color: "rgb(0, 0, 0)",
+    marginLeft: "10px",
+    marginTop: "3px",
+    cursor:"pointer",
+  }
+
+  const closeStyle = {
+    color: "rgb(0, 0, 0)",
+    marginRight: "10px",
+    marginTop: "3px",
+    cursor:"pointer",
+  }
+
+  const deleteTodoList = async(e) => {
+    console.log(`Try to delete listId:${e.target.parentNode.dataset.listid}`)
+  }
+
+  const changeTodoListTitle = async(e) => {
+    console.log(`Try to change title ${text} to ${ETitle} listId:${e.target.parentNode.dataset.listid}`)
+  }
+
+  const onClickTitle = () => {
+    setIsEditingTitle(pre=>!pre)
+    setETitle(text)
+  }
+  const onChangeTitle = (e) => setETitle(e.target.value)
+
+  useEffect(()=>{
+    if(!isEditing)
+      setIsEditingTitle(false)
+  }, [isEditing])
+
   return (
-    <div style={divStyle}>
-      <NavLink
-      style={linkStyle}
-      className={className}
-      to={to}
-      >
-        • {text}
-      </NavLink>
-      <button>
-      삭제
-      </button>
-    </div>
-    
+    <StyledDiv data-listid={listId} isEditing={isEditing}>
+      {!isEditing?
+        <NavLink
+          style={linkStyle}
+          className={className}
+          to={`/todo/${listId}`}
+        >• {text}</NavLink>:
+        !isEditingTitle?
+          <motion.span 
+            style={listNameStyle}
+            whileHover={{
+              scale:1.1
+            }}
+            onClick={onClickTitle}
+          >
+            • {text}
+          </motion.span>:
+          <Input 
+            value={ETitle}
+            onChange={onChangeTitle}
+            style={editStyle}
+          />
+      }
+      {isEditing && !isEditingTitle && (
+        <motion.span 
+          className="material-symbols-outlined" 
+          style={deleteStyle} 
+          onClick={deleteTodoList}
+          whileHover={{
+            color:"rgb(250, 0, 0)", 
+            scale: 1.3
+          }}
+        >
+          delete
+        </motion.span>
+      )}
+
+      {isEditingTitle && isEditing && (
+        <motion.span 
+          className="material-symbols-outlined" 
+          style={checkStyle} 
+          // onClick={deleteTodoList}
+          whileHover={{
+            color:"rgb(0, 150, 0)", 
+            scale: 1.3
+          }}
+          onClick={changeTodoListTitle}
+        >
+          check
+        </motion.span>
+      )}
+
+      {isEditingTitle && isEditing && (
+        <motion.span 
+          className="material-symbols-outlined" 
+          style={closeStyle} 
+          // onClick={deleteTodoList}
+          whileHover={{
+            color:"rgb(200, 50, 50)", 
+            scale: 1.3
+          }}
+          onClick={onClickTitle}
+        >
+          Close
+        </motion.span>
+      )}
+    </StyledDiv>
   );
 }
 
-export default todoList;
+export default TodoList;
