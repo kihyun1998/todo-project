fd3682814bb6c442d38bf91f28d12bb6e7935dbb
Seang-G <mgw7730@gmail.com>
2023-05-18 14:22:11 +0900

    비밀번호변경 스타일 변경

--- src/main/todo-list/src/route/Mypage.jsx
+++ src/main/todo-list/src/route/Mypage.jsx
@@ -6,6 +6,16 @@ import ProfileInfo from "./css/component/ProfileInfo";
 
 import Button from "./css/component/Button";
 import PasswordChange from "./css/component/PasswordChange";
+import { AnimatePresence, motion } from "framer-motion";
+
+const coverStyle = {
+  position: "fixed",
+  width: "100vw",
+  height: "100vh",
+  top:0,
+  left:0,
+  zIndex:1
+}
 
 const Mypage = () => {
   const [userInfo,setUserInfo] = useState({});
@@ -35,6 +45,14 @@ const Mypage = () => {
 
   return (
     <div>
+      <AnimatePresence>
+      {changingPW&&(<motion.div
+        style={coverStyle}
+        initial={{backgroundColor:"rgba(0, 0, 0, 0)"}}
+        animate={{backgroundColor:"rgba(0, 0, 0, 0.3)"}}
+        exit={{backgroundColor:"rgba(0, 0, 0, 0)"}}
+      />)}
+      </AnimatePresence>
       <div>
         <ProfileInfo 
           name = {userInfo.userName}
@@ -47,13 +65,20 @@ const Mypage = () => {
           />
         </span>
       </div>
+      <AnimatePresence>
       {changingPW&& (
-        <div style={{display:"flex", justifyContent:"center"}}>
+        <motion.div 
+          style={{display:"flex", justifyContent:"center", position: "absolute", width:"100%", height: "100%", top:"0%", left: "0%", zIndex:2}}
+          initial={{scale:0}}
+          animate={{scale:1}}
+          exit={{scale:0}}
+        >
           <PasswordChange
             setChangingPW={setChangingPW}
           />
-        </div>
+        </motion.div>
       )}
+      </AnimatePresence>
     </div>
   );
 }
