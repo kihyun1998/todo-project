8c244348a66c2fb67714bcc88ea042ab1a2e43f0
Seang-G <mgw7730@gmail.com>
2023-06-07 22:37:14 +0900

    메인화면 ing1

--- src/main/todo-list/src/route/Intro.jsx
+++ src/main/todo-list/src/route/Intro.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { useCookies } from "react-cookie";
 
 import TodoTable from "./css/component/TodoTable";
-import { AnimatePresence, color, motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 const userNameStyle = {
   fontSize: "3rem",
@@ -12,10 +12,11 @@ const userNameStyle = {
 }
 
 const Intro = () => {
-  const [cookies, setCookie] = useCookies(["accessToken"]);
+  const [cookies, setCookie] = useCookies(["accessToken", "toDoLists"]);
   const [userName, setUserName] = useState("당신");
   const [logined, setLogined] = useState(false);
   const [setting, setSetting] = useState(false);
+  const [mainList, setMainList] = useState(0);
 
   const getUserName = async() => {
     if(cookies.accessToken != null) {
@@ -36,8 +37,6 @@ const Intro = () => {
   return (
     <div className={styles.intro}>
       <h1 style={{marginTop:"100px"}}><span style={userNameStyle}>{userName}</span>의 하루</h1>
-      <h3>사용자 맞춤형 할 일 관리 서비스입니다.</h3>
-
       <AnimatePresence>
       {logined?
         <motion.div
@@ -45,7 +44,7 @@ const Intro = () => {
             backgroundColor: "rgb(245, 245, 245)",
             borderRadius: "30px",
             boxShadow: "7px 7px 10px #737373 inset",
-            minHeight: "50vh",
+            minHeight: "60vh",
             minWidth: "60vw",
             border: "2px solid rgba(0, 0, 0, 0.2)",
             paddingTop: "10px",
@@ -65,7 +64,7 @@ const Intro = () => {
           <div
             style={{
               textAlign: "center",
-              width: "95%",
+              width: "98%",
               display: "flex",
               justifyContent: "end"
 
@@ -94,11 +93,59 @@ const Intro = () => {
                     height: "200px",
                     width: "300px",
                     translateY: "-130%",
-                    backgroundColor: "rgba(10, 10, 10, 0.2)",
+                    backgroundColor: "rgba(240, 240, 240, 0.9)",
                     borderRadius: "20px",
                     boxShadow: "0px 0px 7px grey",
+                    display: "flex",
+                    padding: "20px",
+                  }}
+                  initial={{
+                    scaleY: 0,
+                  }}
+                  animate={{
+                    scaleY: 1,
                   }}
                 >
+                  <div
+                    style={{
+                      display: "flex",
+                      margin: "0 auto",
+                      flexDirection: "column",
+                      alignItems: "baseline",
+                      overflowY: "scroll",
+                      overflowX: "hidden",
+                      width: "80%",
+                      height: "80%",
+                    }}
+                  >
+                    {cookies.toDoLists.map((todoList, idx)=> {
+                      return(
+                        <motion.div 
+                          key={idx} 
+                          style={{
+                            fontSize: "1.2rem",
+                            width: "100%",
+                            cursor: "pointer",
+                            height: "40px"
+                          }}
+                          initial={{
+                            backgroundColor: "rgba(0, 0, 0, 0)",
+                          }}
+                          whileHover={{
+                            backgroundColor: "rgba(0, 0, 0, 0.2)",
+                          }}
+                        >
+                          {todoList.listName}
+                        </motion.div>
+                      );
+                    })}
+                  </div>
+                  <span
+                    className="material-symbols-outlined"
+                  >
+                    check
+                  </span>
+                  
                 </motion.div>
               }
             </div>
@@ -107,7 +154,9 @@ const Intro = () => {
             todos={[]}
           />}
         </motion.div>:
-        <div></div>
+        <div>
+          <h3>사용자 맞춤형 할 일 관리 서비스입니다.</h3>
+        </div>
       }
       </AnimatePresence>
       
