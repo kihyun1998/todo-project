d60756189df9922a96c496a9a49060f18ade447d
redbuttonking <sswedm123@naver.com>
2023-06-09 00:32:27 +0900

    투두 리스트 구현 2

--- src/main/todo-list/src/route/css/component/ToDoTable.jsx
+++ src/main/todo-list/src/route/css/component/ToDoTable.jsx
@@ -1,15 +1,41 @@
 import Todo from "./Todo"
+import { useState } from "react";
 
 const TodoTable = ({todos}) => {
+
+  const [todolist_title,setTodolist_title] = useState("To-Do-List 1");
+
+  
   return (
-    <div>
-      toDoId, todoTitle, estimatedTime, deadline, difficulty, importance, status
+
+    <div>    
+      <h1 style={{marginLeft : "40px" , marginTop:"50px"}}>
+          {todolist_title}
+      </h1>
+       
+      <div
+          style={{
+            width: "93%",
+            marginLeft : "18px",
+            borderBottom: "4px solid #aaa",
+            lineHeight: "0.5em",}}
+      />
+
+      <div style = {{
+        maxHeight : "500px",
+        marginTop : "15px",
+        overflow : a.length > 6 ? "auto" : "none" }}>
+
+
       {todos.map((todo, idx)=>
         <Todo
-          key={idx}
-          todo={todo}
+        key={idx}
+        todo={todo}
         />
-      )}
+        )}
+
+      </div>
+
     </div>
   );
 }
