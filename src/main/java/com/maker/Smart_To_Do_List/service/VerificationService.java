de53227e0b369d667d65b97ec5b6ea23311e64c1
wantSome <kihyun1998@naver.com>
2023-12-11 20:05:15 +0900

    Update VerificationService.java

--- src/main/java/com/maker/Smart_To_Do_List/service/VerificationService.java
+++ src/main/java/com/maker/Smart_To_Do_List/service/VerificationService.java
@@ -22,7 +22,10 @@ public class VerificationService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder encoder;
 
-    // 회원가입 시, 아이디 중복 검증
+    /**
+     [checkLoginIdDup]: 회원가입 시, 아이디 중복 검증
+     loginId: 검증할 아이디
+     **/
     public boolean checkLoginIdDup(String loginId){
         Optional<User> user = userRepository.findByLoginId(loginId);
         if (user.isPresent()){
@@ -31,7 +34,10 @@ public class VerificationService {
         return false;
     }
 
-    // 회원가입 시, 유저이름 중복 검증
+    /**
+     [checkUserNameDup]: 회원가입 시, 유저이름 중복 검증
+     userName: 중복 검증할 유저 이름
+     **/
     public boolean checkUserNameDup(String userName){
         Optional<User> user = userRepository.findByUserName(userName);
         if (user.isPresent()){
@@ -40,7 +46,11 @@ public class VerificationService {
         return false;
     }
 
-    // 접근한 ToDoList에 대해 접근자와 소유자가 동일한지 검증
+    /**
+     [checkListUser]: 접근한 ToDoList에 대해 접근자와 소유자가 동일한지 검증
+     userId: 검증할 소유자 아이디
+     listId: 검증할 리스트 아이디
+     **/
     public void checkListUser(Long userId, Long listId){
         listRepository.findByListId(listId)
                 .ifPresent(list->{
@@ -50,7 +60,12 @@ public class VerificationService {
                 });
     }
 
-    // ToDoList 이름 중복 검증(동일 유저에 한해서)
+    /**
+     [checkListNameDuplicate]: ToDoList 이름 중복 검증(동일 유저에 한해서)
+     userId: 동일 소유자인지 검증할 유저 아이디
+     listName: 중복 검증할 리스트 이름
+     **/
+
     public void checkListNameDuplicate(Long userId, String listName){
         listRepository.findByListName(listName)
                 .ifPresent(list ->{
@@ -60,8 +75,10 @@ public class VerificationService {
                 });
     }
 
-
-    // ToDoList 조회 및 검증
+    /**
+     [foundList]: ToDoList 조회 및 검증
+     listId: 탐색할 리스트 아이디
+     **/
     public ToDoList foundList(Long listId){
         Optional<ToDoList> opToDoList = listRepository.findByListId(listId);
         if (opToDoList.isEmpty()){
@@ -69,8 +86,10 @@ public class VerificationService {
         }
         return opToDoList.get();
     }
-
-    // ToDo 조회 및 검증
+    /**
+     [foundToDo]: ToDo 조회 및 검증
+     toDoId: 탐색할 todo
+     **/
     public ToDo foundToDo(Long toDoId){
         Optional<ToDo> opToDo = toDoRepository.findByToDoId(toDoId);
         if (opToDo.isEmpty()){
@@ -78,8 +97,10 @@ public class VerificationService {
         }
         return opToDo.get();
     }
-
-    // 고유번호를 통해 유저 조회 및 검증
+    /**
+     [foundUser]: 고유번호를 통해 유저 조회 및 검증
+     userId: 탐색할 유저 아이디
+     **/
     public User foundUser(Long userId){
         Optional<User> user = userRepository.findByUserId(userId);
         if (user.isEmpty()){
@@ -87,8 +108,10 @@ public class VerificationService {
         }
         return user.get();
     }
-
-    // 로그인Id를 통해 유저 조회 및 검증
+    /**
+     [foundUserByLoginId]:  로그인Id를 통해 유저 조회 및 검증
+     loginId: 유저를 조회할 로그인 아이디
+     **/
     public User foundUserByLoginId(String loginId){
         Optional<User> user = userRepository.findByLoginId(loginId);
         if (user.isEmpty()){
@@ -96,15 +119,21 @@ public class VerificationService {
         }
         return user.get();
     }
-
-    // 패스워드 검증
+    /**
+     [checkPassword]: 패스워드 검증
+     loginPw: 확인할 비밀번호
+     user: 비밀번호 주인인지 확인할 user 객체
+     **/
     public void checkPassword(String loginPw, User user){
         if(!encoder.matches(loginPw,user.getLoginPw())){
             throw new AppException(ErrorCode.INVALID_PASSWORD, "The password is wrong.");
         }
     }
 
-
+    /**
+     []
+     **/
+    // 미사용 함수
     public ToDoList foundMainList(Long listId){
         Optional<ToDoList> opToDoList = listRepository.findByListId(listId);
         if (opToDoList.isEmpty()){
