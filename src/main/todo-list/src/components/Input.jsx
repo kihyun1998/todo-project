1b1af8f92545a9bf4199067d056078263f8be187
Seang-G <mgw7730@gmail.com>
2024-01-04 19:36:29 +0900

    Input 컴포넌트 수정
    - jsx내의 style을 css module로 분리시킴

--- src/main/todo-list/src/components/Input.jsx
+++ src/main/todo-list/src/components/Input.jsx
@@ -1,44 +1,18 @@
+import styles from "../styles/Input.module.css"
+
 const Input = ({ type, label, value, id, name, onChange, onClick, style, placeholder, DC=false }) => {
 
-  let styleInput = {
-    borderTop: "0",
-    borderLeft: "0",
-    borderRight: "0",
-    borderBottom: "2px solid #d6d6d6",
-    width: "100%",
-    height: "100%",
-    fontFamily: "Sunflower, sans-serif",
-    fontWeight: "1000",
-    color: "#949494",
-    fontSize: "100%",
-    backgroundColor:"rgba(0, 0, 0, 0)"
-    // "backgroundColor": "#fafafa"
-  }
-
-  const styleLabel = {
-    display:"inline-block",
-    width: "200px",
-    fontSize: "1.3rem"
-  }
-
   let styleSpan = {
-    // display:"inline-block",
     height: "50px",
-    // margin:"0 auto",
     width: "fit-content",
-  }
-
-  if (type === "radio") {
-    styleInput["width"] = "20px"
-    styleLabel["width"] = "fit-content"
   }
 
   return (
     <div style={Object.assign({}, styleSpan, style)}>
-      {label&&<label style={styleLabel} htmlFor={id}>{label}</label>}
+      {label&&<label htmlFor={id}>{label}</label>}
       <input
         id={id}
-        style={styleInput}
+        // style={styles.input}
         type={type}
         value={value}
         onChange={onChange}
@@ -46,7 +20,7 @@ const Input = ({ type, label, value, id, name, onChange, onClick, style, placeho
         name = {name}
         defaultChecked = {DC}
         placeholder = {placeholder}
-      ></input>
+      />
     </div>
 
   );
