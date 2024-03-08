d1312bcfa04d1c4380e6ee482bc0ea979bfa7faa
Seang-G <mgw7730@gmail.com>
2024-03-08 15:23:44 +0900

    로딩 화면 수정
    로딩 화면 fade-in fade-out 효과 추가

--- src/main/todo-list/src/components/Loading.jsx
+++ src/main/todo-list/src/components/Loading.jsx
@@ -1,9 +1,23 @@
 import { ClockLoader } from "react-spinners"
 import styles from "../styles/Loading.module.css"
+import { AnimatePresence, motion } from "framer-motion"
 
 export default function Loading({isLoading}) {
-  return(isLoading&&
-    <div className={styles.loading}>
+  return(
+    <AnimatePresence>
+    {isLoading&&
+    <motion.div 
+      className={styles.loading}
+      initial={{
+        opacity: 0
+      }}
+      animate={{
+        opacity: 0.3
+      }}
+      exit={{
+        opacity: 0
+      }}
+    >
       <ClockLoader 
         color="#36d7b7" 
         size={100}
@@ -12,6 +26,8 @@ export default function Loading({isLoading}) {
           animationDelay: "0s",
         }}
       />
-    </div>
+    </motion.div>
+    }
+    </AnimatePresence>
   )
 }
